b7c713fdbd2a8c5f6269087610ffd988020d621b
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-03-26 05:43:50 +0000

    Sync LeetCode submission - Palindrome Partitioning (cpp)

--- leetcode/problems/palindrome_partitioning/solution.cpp
+++ leetcode/problems/palindrome_partitioning/solution.cpp
@@ -1,75 +1,32 @@
-
 class Solution {
 public:
-    bool palin(string s)
-{
-	int i = 0, n = s.length();
-	for(; i < n / 2; i++)
-		if(s[i] != s[n - i - 1])
-			return false;
-	
-	return true;
-	
-}
-vector<deque<string>> doit(string s, int pos, vector<vector<deque<string>>>& store)
-{
-	if(pos == s.length())
-	{
-		vector<deque<string>> another;
-		deque<string> dqs;
-        another.push_back(dqs);
-        // for(auto st: dqs)
-        //     cout << st << '\n';
-		return another;
-	}
-	string cur = "";
-	vector<deque<string>> ans;
-	for(int i = pos; i < s.length(); i++)
-	{
-		cur += s[i];
-		if(palin(cur))
-		{
-            vector<deque<string>> got;
-			if(!store[i + 1][0].empty() && i + 1 != s.length())
-				got = store[i + 1];
-			else 
-            {
-                 got = doit(s, i + 1, store);
-                store[i + 1] = got;
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> res; // which will be our answer
+        vector<string> path; // as we are generating list everythime, so at the end this will be our list
+        helper(0, s, path, res); // calling to recursion function start from index 0 and string s
+        return res;
+    }
+    // Entire recursive function, that generates all the partition substring
+    void helper(int index, string s, vector<string> &path, vector<vector<string>> &res){
+        // Base Condition, which means when we have done partition at the end (n), then add it to our ultimate result
+        if(index == s.size()){
+            res.push_back(path);
+            return;
+        }
+        // Let's talk about partition
+        for(int i = index; i < s.size(); i++){
+            if(isPalindrome(s, index, i)){ // what we are checking over here is, if we partition the string from index to i Example-(0, 0) is palindrome or not
+                path.push_back(s.substr(index, i - index + 1)); // take the substring and store it in our list & call the next substring from index + 1
+                helper(i + 1, s, path, res); // as we have done for (0, 0) then our next will be from (1)
+                path.pop_back(); // please make sure you remove when you backtrack. 
+                // Why? Because let say i had partion y, so when i go back. I can't have yy
             }
-     
-			for(auto& dq: got)
-            {
-                dq.push_front(cur);
-                // for(auto st: dq)
-                //     cout << st << '\n';
-            }
-				
-			ans.insert(ans.end(), got.begin(), got.end());
-		}
-	}
-	return ans;
-}
-
-vector<vector<string>> partition(string s) {
-	
-	vector<vector<deque<string>>> store(s.length() + 4, vector<deque<string>>(1));
-
-	auto ans = doit(s, 0, store);
-    // cout << "df";
-	vector<vector<string>> res;
-	
-	for(auto& dq: ans)
-	{
-		vector<string> fi;
-		while(!dq.empty())
-		{
-			fi.push_back(dq.front());
-			dq.pop_front();
-		}
-		res.push_back(fi);
-	}
-	return res;
-
-}
+        }
+    }
+    bool isPalindrome(string s, int start, int end){ // A simple palindromic function start from 0 go till end. And basically keep on checking till they don't cross. 
+        while(start <= end){
+            if(s[start++] != s[end--]) return false;
+        }
+        return true;
+    }
 };
