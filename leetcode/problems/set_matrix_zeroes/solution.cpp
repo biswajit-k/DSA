121ddeedb2ca2cdce0861afc8f28d3e768fc7f78
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-05-11 11:19:39 +0000

    Sync LeetCode submission - Set Matrix Zeroes (cpp)

--- leetcode/problems/set_matrix_zeroes/solution.cpp
+++ leetcode/problems/set_matrix_zeroes/solution.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
-        int n = matrix.size(), m = matrix[0].size(), isr = 0, isc = 0;
+        
+        int r = 0, c = 0, n = matrix.size(), m = matrix[0].size();
         
         for(int i = 0; i < n; i++)
-        {
-            for(int j = 0; j < m; j++)
-            {
-                if(i == 0 && !matrix[i][j]) isr = 1;
-                if(j == 0 && !matrix[i][j]) isc = 1;
+            if(!matrix[i][0])   r = 1;
+        for(int i = 0; i < m; i++)
+            if(!matrix[0][i])   c = 1;
+        
+        
+        for(int i = 1; i < n; i++)
+            for(int j = 1; j < m; j++)
                 if(!matrix[i][j])
-                    matrix[i][0] = 0, matrix[0][j] = 0;
-            }
-        }
-        for(int i = 1; i < n ;i++)
-            if(!matrix[i][0])
-                for(int j = 0; j < m; j++)  matrix[i][j] = 0;
-        for(int i = 1; i < m ;i++)
-            if(!matrix[0][i])
-                for(int j = 0; j < n; j++)  matrix[j][i] = 0;
-        if(isr)
-            for(int i = 0; i < m; i++)  matrix[0][i] = 0;
-        if(isc)
+                {
+                    matrix[0][j] = 0; matrix[i][0] = 0;
+                }
+                    
+        
+        for(int i = 1; i < n ; i++)
+            for(int j = 1; j < m; j++)
+                if(!matrix[0][j] || !matrix[i][0])  matrix[i][j] = 0;
+        
+        if(r)
             for(int i = 0; i < n; i++)  matrix[i][0] = 0;
+        if(c)   for(int i = 0; i < m; i++)  matrix[0][i] = 0;
+        
     }
 };
