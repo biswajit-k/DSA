889bf52ef4d2c40aeb80bb7dd4423a26b7495fb1
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-05-22 16:54:02 +0000

    Sync LeetCode submission - Sum of Total Strength of Wizards (cpp)

--- leetcode/problems/sum_of_total_strength_of_wizards/solution.cpp
+++ leetcode/problems/sum_of_total_strength_of_wizards/solution.cpp
@@ -2,12 +2,33 @@
 class Solution {
 public:
 
+/* 
+idea - 
+-> maintain previous smaller element using stack
+-> for current element keep poping prev element until it is smaller than current
+	for each greater element keep summing subarrays
+
+
+      p4      p3           p2          p1       i				: a[p2] < a[i] so pop until p3 is top
+	-----------------------------------------------
+                                a[x1]  -- 
+							a[x2]   -----
+						       ----------
+                              at p1, (p2 - p1) values so add a[i] * (p2 - 
+							  p1) for making subarray till i
+
+	-> ans = part less than p3 + part greater than p3 till i
+	-> now, at p3 : ans += ans[p3] + (pref[i] - pref[p3]) needs to be added to all subarray less than p3
+	that is done by : x = (no. of subarrays less than p4) * a[p4] + ...for p3
+
+ */
+
 	const long long mod = 1e9 + 7;
 
     int totalStrength(vector<int>& a) {
 		
 		long long n = a.size(), x = 0;
-
+		// prefix_sum[1...n]
 		vector<long long> prefix(n + 1);
 		prefix[0] = 0;
 		for(int i = 1;i <= n; i++)
