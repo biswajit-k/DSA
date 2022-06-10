fd0f36d44d68ddaf79dceaa952fa1a22440ed4ef
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-06-10 14:05:51 +0000

    Sync LeetCode submission - Basic Calculator II (cpp)

--- leetcode/problems/basic_calculator_ii/solution.cpp
+++ leetcode/problems/basic_calculator_ii/solution.cpp
@@ -1,58 +1,32 @@
 class Solution {
 public:
     
-    unordered_map<char, int> priority = {{'+', 1}, {'-', 1}, {'*', 2}, {'/', 2}};
-    
-    bool isDigit(char x)
-    {
-        return (x >= '0' && x <= '9');
-    }
-    
-    void perform(stack<char>& sign, stack<int>& num)
-    {
-        int o2 = num.top(); num.pop();
-        int o1 = num.top(); num.pop();
-        char op = sign.top(); sign.pop();
-        
-        if(op == '+')   num.push(o1 + o2);
-        else if(op == '-')   num.push(o1 - o2);
-        else if(op == '/')   num.push(o1 / o2);
-        else if(op == '*')   num.push(o1 * o2);
-    }
     
     int calculate(string s) {
         
-        stack<int> num;
-        stack<char> sign;
+        stringstream ss('+' + s);
         
-        for(int i = 0; i < s.length(); i++)
+        int res = 0, last = 0, curr;
+        char op;
+        
+        while(ss >> op >> curr)
         {
-            if(s[i] == ' ')
-                continue;
-            if(isDigit(s[i]))
+            if(op == '+' || op == '-')
             {
-                int x = 0;
-                while(i < s.length() && isDigit(s[i]))
-                {
-                    x *= 10;
-                    x += s[i] - '0';
-                    i++;
-                }
-                i--;
-                num.push(x);
+                res += last;
+                last = (op == '+' ? curr : -curr);
             }
             else 
             {
-                while(!sign.empty() && priority[s[i]] <= priority[sign.top()])
-                    perform(sign, num);
-                sign.push(s[i]);
+                if(op == '*')
+                    last *= curr;
+                else 
+                    last /= curr;
             }
         }
         
-        while(!sign.empty())
-            perform(sign, num);
-        
-        return num.top();
+        res += last;
+        return res;
         
     }
 };
