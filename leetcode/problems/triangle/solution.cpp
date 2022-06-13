0b9b0a1ec8c3bb50b670a2acbfbb87f114d8c24f
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-06-13 09:24:41 +0000

    Sync LeetCode submission - Triangle (cpp)

--- leetcode/problems/triangle/solution.cpp
+++ leetcode/problems/triangle/solution.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
+    
+    const int inf = 1e9;
+
+    
     int minimumTotal(vector<vector<int>>& triangle) {
-    int dp[300][300];
-    dp[1][1]=triangle[0][0];
-    for(int i=2;i<=(int)triangle.size();i++)
-    {
-        for(int j=1;j<=triangle[i-1].size();j++)
+        
+        int n = triangle.size(), prev;
+        vector<int> dis(n + 1, inf);
+        
+        dis[0] = triangle[0][0];
+        
+        for(int r = 1; r < n; r++)
         {
-            if(j==1)
-                dp[i][j]=triangle[i-1][0]+dp[i-1][j];
-            else if(j==triangle[i-1].size())
-                dp[i][j]=dp[i-1][j-1]+triangle[i-1][j-1];
-            else 
+            prev = inf;
+            for(int c = 0; c <= r; c++)
             {
-                dp[i][j]=triangle[i-1][j-1]+min(dp[i-1][j-1],dp[i-1][j]);
+                int tp = dis[c];
+                dis[c] = triangle[r][c] + min(dis[c], prev);
+                prev = tp;
             }
         }
+        
+        return *min_element(begin(dis), end(dis));
     }
-    int minval=10000000;
-    for(int i=1;i<=triangle.size();i++)
-        minval=min(minval,dp[triangle.size()][i]);
-    return minval;
-}
 };
