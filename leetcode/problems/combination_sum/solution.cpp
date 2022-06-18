e4300b9a76debce297aa43534e0a66eea2b52b90
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-06-18 06:48:48 +0000

    Sync LeetCode submission - Combination Sum (cpp)

--- leetcode/problems/combination_sum/solution.cpp
+++ leetcode/problems/combination_sum/solution.cpp
@@ -1,36 +1,40 @@
 class Solution {
 public:
     
-    vector<int> ans;
-    vector<vector<int>> res;
-    // int dp[501][31];
+    using choice = vector<int>;
+    vector<choice> ans;
     
-    void helper(vector<int>& candidates, int target, int pos)
+    void helper(vector<int>& candidates, int target, int pos, choice& ary)
     {
-        if(pos == -1)
-            return;
         if(target == 0)
         {
-            res.push_back(ans);
+            ans.push_back(ary);
             return;
         }
-        // if(dp[target][pos] != -1)
-        if(target >= candidates[pos])
-        {
-            ans.push_back(candidates[pos]);
-            helper(candidates, target - candidates[pos], pos);
-            ans.pop_back();
-        }
-        helper(candidates, target, pos - 1);
+            
         
+        if(pos < 0)
+            return;
+        
+        if(candidates[pos] <= target)
+        {
+            ary.push_back(candidates[pos]);
+            helper(candidates, target - candidates[pos], pos, ary);
+            ary.pop_back();
+        }
+        
+        helper(candidates, target, pos - 1, ary);
+            
     }
     
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+    
+    vector<choice> combinationSum(vector<int>& candidates, int target) {
         
-        // for(int i = 0; i < 501; i++)
-        //     for(int j = 0; j < 31; j++)
-        //         dp[i][j] = -1;
-        helper(candidates, target, size(candidates) - 1);
-        return res;
+        choice ary;
+        
+        helper(candidates, target, candidates.size() - 1, ary);
+        
+        return ans;
+        
     }
 };
