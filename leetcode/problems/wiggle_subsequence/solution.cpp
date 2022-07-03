b7a5b920297b1d6eca521da85dfd5a3c8fea2dfe
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-07-03 08:16:31 +0000

    Sync LeetCode submission - Wiggle Subsequence (cpp)

---
+++ leetcode/problems/wiggle_subsequence/solution.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    
+    
+    int wiggleMaxLength(vector<int>& nums) {
+        
+        int n = nums.size();
+        
+        int dp[n][2];
+        dp[0][0] = dp[0][1] = 1;
+        
+        for(int i = 1; i < n; i++)
+        {
+            dp[i][0] = dp[i][1] = 0;
+            for(int j = i - 1; j > -1; j--)
+            {
+                if(nums[j] > nums[i])
+                {
+                    dp[i][0] = max(dp[i][0], 1 + dp[j][1]);
+                }
+                else if(nums[j] < nums[i])
+                {
+                    dp[i][1] = max(dp[i][1], 1 + dp[j][0]);
+                }
+            }
+        }
+        
+        int ans = 0;
+        for(int i = 0; i < n; i++)
+            ans = max({ans, dp[i][0], dp[i][1]});
+        
+        return ans;
+        
+    }
+};
