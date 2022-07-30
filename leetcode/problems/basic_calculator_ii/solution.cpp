05c8c3316e379f68cca6b140984e90a61d8c389d
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2022-07-30 08:40:58 +0000

    Sync LeetCode submission - Basic Calculator II (cpp)

--- leetcode/problems/basic_calculator_ii/solution.cpp
+++ leetcode/problems/basic_calculator_ii/solution.cpp
@@ -1,32 +1,56 @@
 class Solution {
 public:
     
+    void perform(int& res, char op, int num)
+    {
+        if(op == '+') res += num;
+        else if(op == '-') res -= num;
+        else if(op == '*') res *= num;
+        else if(op == '/') res /= num;
+    }
     
     int calculate(string s) {
+
+        s = '+' + s;
         
-        stringstream ss('+' + s);
+        int n = s.length(), res = 0, p_num = 0, i = 0;
+        char p_op = '+';
         
-        int res = 0, last = 0, curr;
-        char op;
-        
-        while(ss >> op >> curr)
+        while(i < n)
         {
-            if(op == '+' || op == '-')
+            char cur_op;
+            int cur_num = 0;
+            
+            while(i < n && s[i] == ' ') i++;
+            if(i == n)  break;
+            
+            cur_op = s[i];
+            i++;
+            
+            while(i < n && s[i] == ' ') i++;
+            while(i < n && s[i] >= '0' && s[i] <= '9')
             {
-                res += last;
-                last = (op == '+' ? curr : -curr);
+                cur_num = cur_num * 10 + (s[i] - '0');
+                i++;
             }
-            else 
+            
+            if(p_op == '*' || p_op == '/' || cur_op == '+' || cur_op == '-')
             {
-                if(op == '*')
-                    last *= curr;
+                perform(res, p_op, p_num);
+                p_op = cur_op;
+                p_num = cur_num;
+            }
+            else
+            {
+                if(cur_op == '*')
+                    p_num *= cur_num;
                 else 
-                    last /= curr;
+                    p_num /= cur_num;
             }
+            
         }
+        perform(res, p_op, p_num);
         
-        res += last;
         return res;
-        
     }
 };
