a45a511cb4d9e646c656b04e8b0787f6ade11ffc
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-04-17 05:16:58 +0000

    Sync LeetCode submission - Kids With the Greatest Number of Candies (cpp)

--- leetcode/problems/kids_with_the_greatest_number_of_candies/solution.cpp
+++ leetcode/problems/kids_with_the_greatest_number_of_candies/solution.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
-    vector<bool> kidsWithCandies(vector<int>& candies, int extraCandies) {
-        vector<bool> x;
-        for(int i=0;i<candies.size();++i){
-            int f=-1;
-            for(int j=0;j<candies.size();++j){
-                if(candies[i]+extraCandies<candies[j])  {f=0; break;}
-                
-            }
-            if(f==-1)   {x.push_back(1);}
-            else    {x.push_back(0);}
-        }
-        return x;
+    vector<bool> kidsWithCandies(vector<int>& candies, int extra) {
+        
+        int n = candies.size();
+        int mx = *max_element(begin(candies), end(candies));
+
+        vector<bool> result(n);
+        for(int i = 0; i < n; i++)
+            result[i] = (candies[i] + extra >= mx);
+        
+        return result;
     }
-    
 };
