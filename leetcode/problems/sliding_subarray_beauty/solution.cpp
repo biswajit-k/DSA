3f7cce69870fdf0ed41b06096b53070d28158bc4
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-04-23 06:59:49 +0000

    Sync LeetCode submission - Sliding Subarray Beauty (cpp)

--- leetcode/problems/sliding_subarray_beauty/solution.cpp
+++ leetcode/problems/sliding_subarray_beauty/solution.cpp
@@ -1,52 +1,74 @@
 class Solution {
 public:
-    vector<int> getSubarrayBeauty(vector<int>& nums, int k, int x) {
+    
+    /*
+            fenwick tree depend on number range
+                [-1e6, 1e6] => add offset => new range [0, 2e6]
+                    time -> O(logN)
+                any range but numbers known => map
         
-        int n = nums.size();
+    */
+    
+    void add(int idx, int v, vector<int>& bit) {
         
-        vector<int> res;
-        if(k == 1)
+        for(; idx < bit.size(); idx += (idx & -idx))
+            bit[idx] += v;
+    }
+    
+    int sum(int idx, vector<int>& bit) {
+        
+        int ans = 0;
+        for(; idx > 0; idx -= (idx & -idx))
+            ans += bit[idx];
+        return ans;
+    }
+    
+    int search(int pos, vector<int>& bit, vector<int>& dup) {
+        
+        int l = 1, r = bit.size() - 1;
+        
+        while(l < r) 
         {
-            res = nums;
-            for(int i = 0; i < n; i++)
-                if(res[i] > 0)
-                    res[i] = 0;
-            return res;
-        }
-        multiset<int> st;
-        queue<multiset<int>::iterator> q;
-        int pos = 0;
-        for(; pos < k; pos++)
-        {
-            auto i = st.insert(nums[pos]);
-            q.push(i);
-        }
+            int m = (l + r) / 2;
             
-        
-        auto it = st.begin();
-        for(int i = 0; i < x - 1; i++)
-            it++;
-        
-        res.push_back(min(0, *it));
-        for(; pos < n; pos++)
-        {
-            auto last = q.front();
-            q.pop();
-            int ins = nums[pos];
+            int s = sum(m, bit);
             
-            auto it2 = it;
-            if(*last <= *it)
-                it2++;
-            
-            st.erase(last);
-            
-            auto i = st.insert(ins);
-            q.push(i);
-            if(it2 == st.end() || ins < *it2)
-                it = --it2;
+            if(s < pos)
+                l = m + 1;
             else 
-                it = it2;
-            res.push_back(min(0, *it));
+                r = m;
+        }
+        return min(0, dup[l - 1]);
+    }
+    
+    
+    vector<int> getSubarrayBeauty(vector<int>& A, int k, int x) {
+        
+        int n = A.size();
+        
+        
+        vector<int> dup = A;
+        sort(begin(dup), end(dup));
+        dup.resize(unique(begin(dup), end(dup)) - begin(dup));
+        
+        int m = dup.size();
+        
+        vector<int> bit(m + 1, 0), res;
+        unordered_map<int, int> mp;
+        for(int i = 0; i < m; i++)
+            mp[dup[i]] = i + 1;
+        
+        for(int i = 0; i < k; i++)
+            add(mp[A[i]], 1, bit);
+        
+        res.push_back(search(x, bit, dup));
+        
+        for(int i = k; i < n; i++)
+        {
+            add(mp[A[i - k]], -1, bit);
+            add(mp[A[i]], 1, bit);
+            
+            res.push_back(search(x, bit, dup));
         }
         
         return res;
