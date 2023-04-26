df8936b16d32fa93f054f800788b69aba5e9c2f8
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-04-26 11:17:18 +0000

    Sync LeetCode submission - Add Digits (cpp)

---
+++ leetcode/problems/add_digits/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int addDigits(int num) {
+        
+        int temp = 0;
+        while(num > 9)
+        {
+            while(num)
+            {
+                temp += num % 10;
+                num /= 10;
+            }
+            num = temp;
+            temp = 0;
+        }
+
+        return num;
+    }
+};
