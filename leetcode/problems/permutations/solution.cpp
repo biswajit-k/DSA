51651bacc1e8c0139ba6d1910c904d886b117aec
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-05-05 09:14:31 +0000

    Sync LeetCode submission - Permutations (cpp)

--- leetcode/problems/permutations/solution.cpp
+++ leetcode/problems/permutations/solution.cpp
@@ -1,44 +1,29 @@
 class Solution {
 public:
+
+    void helper(int pos, vector<int>& nums, vector<vector<int>>& res) {
+
+        if(pos == nums.size())
+        {
+            res.push_back(nums);
+            return;
+        }
+        for(int i = pos; i < nums.size(); i++)
+        {
+            swap(nums[pos], nums[i]);
+            helper(pos + 1, nums, res);
+            swap(nums[pos], nums[i]);
+        }
+
+    }
+
+
     vector<vector<int>> permute(vector<int>& nums) {
         
-        vector<vector<int>> ans;
-        
-        int cnt = 1;
-        for(int i = 2; i <= nums.size(); i++)
-            cnt *= i;
-        
-        for(int i = 0; i < cnt; i++)
-        {
-            next(nums);
-            ans.push_back(nums);
-        }
-        return ans;
-    }
-    
-    void next(vector<int>& nums){
-        int n = nums.size();
-        if(n <= 1)
-            return;
-        
-        int pos = n - 2;
-        for(; pos > -1; pos--)
-        {
-            if(nums[pos] < nums[pos + 1])
-                break;
-        }
-        if(pos == -1)
-        {
-            reverse(nums.begin(), nums.end());
-            return;
-        }
-        
-        reverse(nums.begin() + pos + 1, nums.end());
-        for(int i = pos + 1; i < n; i++)
-            if(nums[i] > nums[pos])
-            {
-                swap(nums[i], nums[pos]);
-                break;
-            }
+        vector<vector<int>> res;
+        sort(begin(nums), end(nums));
+
+        helper(0, nums, res);
+        return res;
     }
 };
