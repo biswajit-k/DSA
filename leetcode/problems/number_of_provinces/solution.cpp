91ec67cf5a38689bd7175df3a6b5ea604065ff2a
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-06-04 05:54:15 +0000

    Sync LeetCode submission - Number of Provinces (cpp)

--- leetcode/problems/number_of_provinces/solution.cpp
+++ leetcode/problems/number_of_provinces/solution.cpp
@@ -1,45 +1,23 @@
-
 class Solution {
 public:
-    
-    int parent[201], sz[201];
 
-    int get_parent(int x)
-    {
-        if(parent[x] == x)  return x;
-        return parent[x] = get_parent(parent[x]);
+    void dfs(int x, vector<vector<int>>& matrix) {
+
+        matrix[x][x] = 0;
+        for(int i = 0; i < matrix[x].size(); i++)
+            if(matrix[x][i] && matrix[i][i])
+                dfs(i, matrix);
+                
     }
-    
-    void group(int a, int b)
-    {
-        int pa = get_parent(a);
-        int pb = get_parent(b);
-        if(pa == pb)    return;
 
-        if(sz[pb] > sz[pa]) 
-        {
-            swap(a, b);
-            swap(pa, pb);
-        }
-
-        parent[pb] = pa;
-        sz[pa] += sz[pb];
-    }
-    
     int findCircleNum(vector<vector<int>>& isConnected) {
         
-        int n = isConnected.size();
+        int components = 0;
 
-        for(int i = 0; i < n;i++)
-            parent[i] = i, sz[i] = 1;
+        for(int i = 0; i < size(isConnected); i++)
+            if(isConnected[i][i])
+                dfs(i, isConnected), components++;
 
-        for(int i = 0; i < n;i++)
-            for(int j = 0; j < n; j++)
-                if(isConnected[i][j])   group(i, j);
-
-        int cnt = 0;
-        for(int i = 0; i < n; i++)  if(parent[i] == i)  cnt++;
-        return cnt;
-
+        return components;
     }
 };
