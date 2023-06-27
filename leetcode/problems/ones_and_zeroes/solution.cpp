4409e7ab4e6ae260c34bf33d4c7449287b47bae7
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-06-27 08:51:45 +0000

    Sync LeetCode submission - Ones and Zeroes (cpp)

--- leetcode/problems/ones_and_zeroes/solution.cpp
+++ leetcode/problems/ones_and_zeroes/solution.cpp
@@ -1,25 +1,23 @@
 class Solution {
 public:
-    
-    int dp[101][101];
-    
     int findMaxForm(vector<string>& strs, int m, int n) {
-      
-        for(int i = 0; i < 101; i++)
-            for(int j = 0; j < 101; j++)
-                dp[i][j] = 0;
         
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
         for(int i = 0; i < strs.size(); i++)
         {
-            int zero = count(begin(strs[i]), end(strs[i]), '0');
-            int one = strs[i].length() - zero;
-            
-            for(int j = m; j >= zero; j--)
-                for(int k = n; k >= one; k--)
-                    dp[j][k] = max(dp[j][k], 1 + dp[j - zero][k - one]);
-            
+            int z = count(begin(strs[i]), end(strs[i]), '0');
+            int o = strs[i].size() - z;
+
+            for(int j = m; j >= z; j--)
+            {
+                for(int k = n; k >= o; k--)
+                {
+                    dp[j][k] = max(dp[j][k], 1 + dp[j - z][k - o]);
+                }
+            }
         }
-        
+
         return dp[m][n];
     }
 };
