9eec2a848a7248fa58320ac6cf755e37e88238a6
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-06-29 10:59:24 +0000

    Sync LeetCode submission - Shortest Path to Get All Keys (cpp)

--- leetcode/problems/shortest_path_to_get_all_keys/solution.cpp
+++ leetcode/problems/shortest_path_to_get_all_keys/solution.cpp
@@ -1,107 +1,45 @@
 class Solution {
-
-    const int inf = 1e8;
-
-    struct Point {
-        int x, y;
-
-        Point(int a = 0, int b = 0) {
-            x = a, y = b;
-        }
-
-    };
-
-    int dx[4] = {0, 1, -1, 0};
-    int dy[4] = {1, 0, 0, -1};
-
-    unordered_map<char, Point> coordinates;
-
-    bool is_valid(int& x, int& y, unordered_set<char>& not_available, vector<vector<bool>>& vis, vector<string>& grid) {
-        if(min(x, y) >= 0 && x < grid.size() && y < grid[0].size())
-        {
-            if(grid[x][y] == '#' || (grid[x][y] <= 'Z' && grid[x][y] >= 'A' && not_available.count(grid[x][y] + 'a' - 'A')) || vis[x][y])
-            return 0;
-            return 1;
-        }
-        return 0;
-    }
-
-    int get_path_length(int x, int y, char final, unordered_set<char>& not_available, vector<string>& grid) {
-        if(grid[x][y] == final)
-            return 0;
-
-        queue<pair<int, int>> q;
-        vector<vector<bool>> vis(grid.size(), vector<bool>(grid[0].size(), false));
-        q.push({x, y});
-        vis[x][y] = true;
-
-        int l = 1;
-
-        while(!q.empty())
-        {
-            int sz = q.size();
-            for(int i = 0; i < sz; i++)
-            {
-                auto [x, y] = q.front();
-                q.pop();
-                for(int i = 0; i < 4; i++)
-                {
-                    int xx = x + dx[i], yy = y + dy[i];
-                    if(is_valid(xx, yy, not_available, vis, grid))
-                    {
-                        if(grid[xx][yy] == final)   return l;
-                        q.push({xx, yy});
-                        vis[xx][yy] = true;
-                    }
-                }
-            }
-            l++;
-        }
-
-        return inf;
-    }
-
 public:
-
-    int shortest_path(Point& start, string& keys, int pos, vector<string>& grid) {
-        if(pos == keys.size())
-            return 0;
-
-        unordered_set<char> not_available(begin(keys) + pos, end(keys));
-
-        int ans = inf;
-
-        for(int i = pos; i < keys.length(); i++)
-        {
-            swap(keys[pos], keys[i]);
-
-            int path_length = get_path_length(start.x, start.y, keys[pos], not_available, grid);
-            int remaining_path_length = shortest_path(coordinates[keys[pos]], keys, pos + 1, grid);
-            // cout << pos << ' ' << path_length << ' ' << remaining_path_length << '\n';
-            ans = min(ans, path_length + remaining_path_length);
-
-            swap(keys[pos], keys[i]);
-        }
-
-        return ans;
-    }
-
     int shortestPathAllKeys(vector<string>& grid) {
-        
-        int n = grid.size(), m = grid[0].size();
-        Point start;
-        string keys = "";
-
-        for(int i = 0; i < n; i++)
-            for(int j = 0; j < m; j++)
-            {
-                if(grid[i][j] == '@')
-                    start.x = i, start.y = j;
-                else if(grid[i][j] <= 'z' && grid[i][j] >= 'a')
-                    keys += grid[i][j], coordinates[grid[i][j]] = Point(i, j);
+    int m=grid.size(), n=m?grid[0].size():0;
+    if(!m || !n) return 0;
+    int path=0, K=0;
+    vector<int> dirs={0,-1,0,1,0};
+    vector<vector<vector<bool>>> visited(m,vector<vector<bool>>(n,vector<bool>(64,0))); //at most 6 keys, using bitmap 111111
+    queue<pair<int,int>> q; //<postion, hold keys mapping>
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            if(grid[i][j]=='@'){
+                q.push({i*n+j,0});
+                visited[i][j][0]=1;                    
             }
-
-        int len = shortest_path(start, keys, 0, grid);
-        return (len >= inf ? -1 : len);
+            if(grid[i][j]>='A' && grid[i][j]<='F') K++; //total alpha number
+        }
     }
+    while(!q.empty()){
+        int size=q.size();
+        for(int i=0;i<size;i++){
+            int a=q.front().first/n, b=q.front().first%n;
+            int carry=q.front().second;
+            q.pop();        
+            if(carry==((1<<K)-1)) return path; //if all keys hold, just return 
+            for(int j=0;j<4;j++){
+                int x=a+dirs[j], y=b+dirs[j+1], k=carry;
+                if(x<0 || x>=m || y<0 || y>=n || grid[x][y]=='#') continue;
+                if(grid[x][y]>='a' && grid[x][y]<='f'){
+                    k=carry|(1<<(grid[x][y]-'a')); //update hold keys
+                }
+                else if(grid[x][y]>='A' && grid[x][y]<='F'){
+                    if(!(carry & (1<<(grid[x][y]-'A')))) continue;
+                }
+                if(!visited[x][y][k]){
+                    visited[x][y][k]=1;
+                    q.push({x*n+y,k});
+               }                
+            }
+        }
+        path++;
+    }
+    return -1;
+}
 };
