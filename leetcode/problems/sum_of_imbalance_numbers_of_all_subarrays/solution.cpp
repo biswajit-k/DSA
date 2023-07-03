c4a6bb9803490912ddd32278cf0b7f8af124bb37
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-07-03 12:34:18 +0000

    Sync LeetCode submission - Sum of Imbalance Numbers of All Subarrays (cpp)

--- leetcode/problems/sum_of_imbalance_numbers_of_all_subarrays/solution.cpp
+++ leetcode/problems/sum_of_imbalance_numbers_of_all_subarrays/solution.cpp
@@ -1,44 +1,25 @@
 class Solution {
 public:
     
-    int get_diff(multiset<int>::iterator& it, multiset<int>& st) {
-        
-        auto i1 = st.end(), i2 = st.end();
-        if(it != st.begin())
-            i1 = prev(it);
-        i2 = next(it);
-        
-        int diff = 0;
-        if(i1 != st.end())
-            diff += (*it - *i1 > 1);
-        if(i2 != st.end())
-            diff += (*i2 - *it > 1);
-            
-        if(i1 != st.end() && i2 != st.end())
-            diff -= (*i2 - *i1 > 1);
-        
-        return diff;
-    }
-    
-    
     int sumImbalanceNumbers(vector<int>& nums) {
         
         int n = nums.size();
-        
-        int ans = 0;
+        vector<int> left(n + 2, -1), right(n + 2, n);
+        vector<int> ans(n);
+
         for(int i = 0; i < n; i++)
         {
-            multiset<int> st;
-            st.insert(nums[i]);
-            int curr_ans = 0;
-            for(int j = i + 1; j < n; j++)
-            {
-                auto it = st.insert(nums[j]);
-                curr_ans += get_diff(it, st);
-                ans += curr_ans;
-            }
+            ans[i] = i - left[nums[i] + 1];
+            left[nums[i]] = i;
+        }
+        for(int i = n - 1; i > -1; i--)
+        {
+            int idx = min(right[nums[i]], right[nums[i] + 1]);
+            ans[i] *= idx - i;
+            right[nums[i]] = i;
         }
         
-        return ans;
+        int res = accumulate(begin(ans), end(ans), 0) - n * (n + 1) / 2;
+        return res;
     }
 };
