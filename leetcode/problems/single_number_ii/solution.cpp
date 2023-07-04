5f7b2efe0c01cd86c87f90b8f14dfaad0c1f0028
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-07-04 09:46:52 +0000

    Sync LeetCode submission - Single Number II (cpp)

---
+++ leetcode/problems/single_number_ii/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+        int x1 = 0, x2 = 0, mask;
+
+        for(auto& i: nums) 
+        {
+            x2 ^= x1 & i;
+            x1 ^= i;
+
+            mask = ~(x1 & x2);
+            x2 &= mask;
+            x1 &= mask;
+        }
+
+        return x1 | x2;
+    }
+};
