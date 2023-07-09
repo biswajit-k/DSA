e84f8a8bdf857eb71b09777e40c1458fb27a284e
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-07-09 02:37:17 +0000

    Sync LeetCode submission - Maximum Number of Jumps to Reach the Last Index (cpp)

---
+++ leetcode/problems/maximum_number_of_jumps_to_reach_the_last_index/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    
+    const int inf = 1e9;
+    
+    int maximumJumps(vector<int>& nums, int target) {
+        
+        int n = nums.size();
+        vector<int> dp(n, -1);
+        dp[n - 1] = 0;
+        
+        for(int i = n - 2; i > -1; i--)
+        {
+            
+            for(int j = i + 1; j < n; j++)
+                if(dp[j] != -1 && abs(nums[i] - nums[j]) <= target)
+                    dp[i] = max(dp[i], 1 + dp[j]);
+        }
+        
+        return dp[0];
+    }
+};
