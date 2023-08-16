532e0da0a0eb82f2cc1bb73f475cd1c4e5930a6d
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-08-16 11:54:58 +0000

    Sync LeetCode submission - Sliding Window Maximum (cpp)

--- leetcode/problems/sliding_window_maximum/solution.cpp
+++ leetcode/problems/sliding_window_maximum/solution.cpp
@@ -1,29 +1,33 @@
 class Solution {
 public:
+
+    void operation(int i, vector<int>& nums, deque<int>& q) {
+        while(!q.empty() && nums[q.back()] <= nums[i])
+            q.pop_back();
+        q.push_back(i);
+    }
+
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> d;
-        int mx=INT_MIN,i=0;
+
         vector<int> ans;
-        for(i=0;i<k;i++){
-            
-            //remove elements smaller than the current on left 
-            while(!d.empty() && nums[d.back()]<=nums[i]) d.pop_back();
-            d.push_back(i);
+        deque<int> q;
+
+        for(int i = 0; i < k - 1; i++)
+            operation(i, nums, q);
+
+        for(int i = k - 1; i < nums.size(); i++)
+        {
+            operation(i, nums, q);
+
+            if(q.front() < i - k + 1)
+                q.pop_front();
+
+            ans.push_back(nums[q.front()]);
         }
-        ans.push_back(nums[d.front()]);
-        
-        for(;i<nums.size();i++){
-            //remove elements not in window
-            while(!d.empty() && d.front()<=i-k) d.pop_front();
-            
-            //remove elemnts smaller than the current on left
-            while(!d.empty() && nums[d.back()]<=nums[i]) d.pop_back();
-            
-            //add the current element and store answer
-            d.push_back(i);
-            ans.push_back(nums[d.front()]);
-            
-        }
-        
-        
-        return ans;}};
+
+        return ans;
+    }
+};
+
+
+// 1, 3, -1, -3, 5, 3, 6, 7
