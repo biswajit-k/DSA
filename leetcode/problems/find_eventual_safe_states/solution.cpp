3d1b4975ce5ad5f9e675191716f683cf077bad35
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-09-04 10:36:38 +0000

    Sync LeetCode submission - Find Eventual Safe States (cpp)

--- leetcode/problems/find_eventual_safe_states/solution.cpp
+++ leetcode/problems/find_eventual_safe_states/solution.cpp
@@ -4,22 +4,15 @@ public:
     void helper(int x, vector<vector<int>>& graph, vector<int>& type) {
 
         type[x] = 1;
-        if(graph[x].empty())
-        {
-            type[x] = 2;
-            return;
-        }
         for(auto& c: graph[x])
         {
             if(!type[c])
                 helper(c, graph, type);
             
             if(type[c] != 2)
-            {
-                type[c] = 3;
                 return;
-            }
         }
+
         type[x] = 2;
     }
 
