a83d4bef0bcb506f0bab00564cf57af5212e032f
Biswajit Kaushik <76483357+biswajit-k@users.noreply.github.com>
2023-09-10 16:32:42 +0000

    Sync LeetCode submission - Points That Intersect With Cars (cpp)

--- leetcode/problems/points_that_intersect_with_cars/solution.cpp
+++ leetcode/problems/points_that_intersect_with_cars/solution.cpp
@@ -3,16 +3,23 @@ public:
     int numberOfPoints(vector<vector<int>>& nums) {
         
         int n = nums.size(), pts = 0;
-        unordered_set<int> taken;
+        
+        sort(begin(nums), end(nums));
+        
+        int least_start = 0;
         
         for(int i = 0; i < n; i++)
         {
-            for(int j = nums[i][0]; j <= nums[i][1]; j++)
-                if(!taken.count(j))
-                {
-                    taken.insert(j);
-                    pts++;
-                }
+            if(nums[i][0] >= least_start)
+            {
+                pts += nums[i][1] - nums[i][0] + 1;
+                least_start = nums[i][1] + 1;
+            }
+            else
+            {
+                pts += max(0, nums[i][1] - least_start + 1);
+                least_start = max(least_start, nums[i][1] + 1);
+            }
         }
         
         return pts;
